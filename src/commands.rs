2f5d279cf8f87f2104dc8355a6f0302e53feaed5
Jamason Davis <jpdavis2471@gmail.com>
2021-02-02 07:35:29 -0600

    Added CURL support, requires local deve webserver

--- src/commands.rs
+++ src/commands.rs
@@ -1,7 +1,24 @@
-pub fn install() {
-    print!("test");
-}
+use std::fs;
+use curl::easy::Easy;
 
 pub fn update() {
-    print!("test");
+    print!("Updating Repositories");
+    let mut easy = Easy::new();
+    easy.url("http://127.0.0.1/repoheader").unwrap();
+    easy.write_function(|data| {
+        fs::write("repoheader", data).expect("Unable to read file");
+        Ok(data.len())
+    }).unwrap();
+    easy.perform().unwrap()
+}
+
+pub fn install() {
+    update();
+    let mut easy = Easy::new();
+    easy.url("http://127.0.0.1/test.repo").unwrap();
+    easy.write_function(|data| {
+        fs::write("test.repo", data).expect("Unable to read file");
+        Ok(data.len())
+    }).unwrap();
+    easy.perform().unwrap()
 }
