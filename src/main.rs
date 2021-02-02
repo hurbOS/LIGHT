44c26618bfc476201cf7bde6853fc438817a3d3b
Jamason Davis <jpdavis2471@gmail.com>
2021-02-01 21:07:29 -0600

    Created basic help menu, basic argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+
+fn help() {
+    print!("LIGHT Help\n\n\nCommands:\n\nInstall: --install/-i [package]\nSearch, download, and install a package from enabled repositories.")
+}
+
+fn command_parse() {
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+    if command == "-i" || command == "--install" {
+        print!("Test")
+    } else {
+        print!("Invalid command")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        2 => {
+            command_parse();
+        },
+        _ => {
+            help();
+        }
+    }
 }
